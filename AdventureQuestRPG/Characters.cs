789b836a858456e73d12108693123bd82f9f1ba2
loiy <lloiy9310@gmail.com>
2024-07-01 12:40:21 +0300

    ADD monster classes

---
+++ AdventureQuestRPG/Characters.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace AdventureQuestRPG
+{
+
+    //player class
+
+
+    public abstract class Monster
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int Defense { get; set; }
+
+        public Monster(string name, int health, int attackPower, int defense)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+            Defense = defense;
+        }
+
+        public abstract void Attack(Player player);
+    }
+
+    public class Skullton : Monster
+    {
+        public Skullton(string name = "Skullton", int health = 90, int attackPower = 10, int defense = 10)
+            : base(name, health, attackPower, defense) { }
+
+        public override void Attack(Player player)
+        {
+           BattleSystem.Attack(this, player);
+        }
+    }
+
+    public class Zombie : Monster
+    {
+        public Zombie(string name = "Zombie", int health = 70, int attackPower = 10, int defense = 6)
+            : base(name, health, attackPower, defense) { }
+
+        public override void Attack(Player player)
+        {
+            BattleSystem.Attack(this, player);
+        }
+    }
+
+    public class Goblin : Monster
+    {
+        public Goblin(string name = "Goblin", int health = 80, int attackPower = 15, int defense = 5)
+            : base(name, health, attackPower, defense) { }
+
+        public override void Attack(Player player)
+        {
+            BattleSystem.Attack(this, player);
+        }
+    }
+}
--- AdventureQuestRPG/Player.cs
+++ AdventureQuestRPG/Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdventureQuestRPG
 {
-    internal class Player
+    public class Player
     {
         //Fields 
         private string name;
